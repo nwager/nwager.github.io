5f92aaf8b0fa742cdd4e76891e189d877cb672c1
Noah Wager <nwager@cs.washington.edu>
2022-01-24 10:36:23 -0800

    update conditions

--- push.sh
+++ push.sh
@@ -3,13 +3,20 @@ function usage {
   exit 1
 }
 
+if (( $# < 1 || $# > 2 )); then
+  usage;
+fi
+
+MSG="$1"
+
 # check for -A flag to git add all
 if [ $# == 2 ]; then
   if [ "$1" != "-A" ]; then
     usage;
   else
-    git add -A;
+    MSG="$2"
+    git add -A || exit 1;
   fi
 fi
 
-git commit -m "$1" && git push && npm run deploy
+git commit -m "$MSG" && git push && npm run deploy
